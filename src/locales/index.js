85518d5c0cb31e4e776848071678154eb19938d7
SUNWUYUAN <1847261658@qq.com>
2023-05-01 18:02:20 +0800

    1

--- src/locales/index.js
+++ src/locales/index.js
@@ -35,12 +35,12 @@ const KNOWN_GOOD_LANGUAGES = ['en', 'es', 'ja', 'nl', 'it', 'sl'];
 const getInitialLocale = () => [
   navigator.language.toLowerCase(),
   navigator.language.toLowerCase().split('-')[0]
-].find(i => KNOWN_GOOD_LANGUAGES.includes(i) && allMessages[i]) || 'en';
+].find(i => KNOWN_GOOD_LANGUAGES.includes(i) && allMessages[i]) || 'zh-cn';
 
 const locale = writablePersistentStore('P4.locale', getInitialLocale());
 locale.subscribe((lang) => {
   if (!allMessages[lang]) {
-    locale.set('en');
+    locale.set('zh-cn');
   }
   document.documentElement.lang = lang;
 });
