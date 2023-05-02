30f24a4e644278205fef321ea04ca987c3777d46
SUNWUYUAN <1847261658@qq.com>
2023-05-02 15:49:52 +0800

    1

--- src/p4/get-project-metadata.js
+++ src/p4/get-project-metadata.js
@@ -6,7 +6,8 @@ const getProjectMetadata = async (id) => {
     const meta = await request({
       url: [
         // Hopefully one of these URLs won't be blocked.
-        `https://ourworld.wuyuan.dev/scratch/play/project/${id}`
+        `https://trampoline.turbowarp.org/api/projects/${id}`,
+        `https://trampoline.turbowarp.xyz/api/projects/${id}`
       ],
       type: 'json'
     });
